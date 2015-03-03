5d17f2bdca193134a90062048fd5c32b2923dbef
Your Name <Tahani@rgnt2-116-105-dhcp.int.colorado.edu>
2015-03-02 17:19:16 -0700

    add some files

---
+++ contents/index.md
@@ -0,0 +1,4 @@
+---
+title: Twitter
+template: layout.jade
+---
---
+++ contents/js/app.js
@@ -0,0 +1,17 @@
+$( "#tweets" ).on( "click", function( event ) {
+    tweets.load()
+})
+
+$( "#followers" ).on( "click", function( event ) {
+    followers.load()
+})
+
+$( "#friends" ).on( "click", function( event ) {
+    friends.load()
+})
+
+$( "#favorites" ).on( "click", function( event ) {
+    favorites.load()
+})
+
+
