1a4d784f7cf258dbfa8cf5d35e896249e41bac07
spencehood <spho6674@colorado.edu>
2015-03-04 18:38:05 -0700

    changing contents>friends code

--- contents/friends/index.js
+++ contents/friends/index.js
@@ -76,3 +76,44 @@ var legislators = {
     }
 
 }
+
+
+
+
+
+var friends = {
+
+    searchByFriends: function(title) {
+
+        title.replace(" ", "%20")
+
+        $.get("https://api.spotify.com/v1/search?q="+title+"&type=album", function(data) {
+
+            console.log(data.friends.items)
+            if (data){
+                $.get("/cat-spotify/albums/list.jade", function(template) {
+                    var html = jade.render(template, {
+                        data: data
+                    })
+                    console.log(html)
+                    $("#list").html(html)
+                })
+            }
+
+        })
+
+    },
+
+    load: function() {
+
+        $.get("/twitter-jquery/contents/friends/ui.jade", function(template) {
+            var html = jade.render(template)
+            $("#searchdiv").html(html)
+        })
+
+        // default search results
+        albums.searchByFriends('Babel')
+
+    }
+
+}
